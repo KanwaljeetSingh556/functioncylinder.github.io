ddfff7a438a6d77c2643b7a86dcd4d26bb09489a
Kawal22 <92438895+Kawal22@users.noreply.github.com>
2021-11-14 14:46:12 +0530

    Add files via upload

---
+++ function-cylinder.js
@@ -0,0 +1,7 @@
+
+let cylinder = (radius,height) => {
+    let volume = (3.14*radius*radius)
+    let surfacearea = ((2*3.14*radius*height) + (2*3.14*radius*radius) )
+    console.log("volume of cylinder is" + volume)
+    console.log("surface area of cylinder is" + surfacearea)
+}
